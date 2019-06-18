7f292647985255b60144e92bf25acff19446b54a
yliess86 <hatiyliess86@gmail.com>
2019-06-18 18:56:49 +0200

    Change model path in sh

--- api.sh
+++ api.sh
@@ -6,9 +6,9 @@ pip install -r requirements.txt
 
 python3 api.py \
   -d cuda \
-  -g '/Projects/PaintsTorchExp/paper_random_simpler/generator.pth' \
-     '/Projects/PaintsTorchExp/paper_strokes_simpler/generator.pth' \
-     '/Projects/PaintsTorchExp/paper_strokes_double/generator.pth' \
-     '/Projects/PaintsTorchExp/custom_strokes_double/generator.pth' \
+  -g '/Projects/PaintsTorchExp/trained/prs/generator.pth' \
+     '/Projects/PaintsTorchExp/trained/pss/generator.pth' \
+     '/Projects/PaintsTorchExp/trained/psd/generator.pth' \
+     '/Projects/PaintsTorchExp/trained/csd/generator.pth' \
   -n 'PRS' 'PSS' 'PSD' 'CSD' \
   -i '/Projects/PaintsTorch/res/model/i2v.pth'
