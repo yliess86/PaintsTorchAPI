bf09b6f543425331e0f13a8c70b577b26a62965d
yliess86 <hatiyliess86@gmail.com>
2019-06-19 23:26:17 +0200

    Add support for study results

--- mos.py
+++ mos.py
@@ -39,7 +39,7 @@ def get_results(db, cursor):
                     (SELECT rate FROM study GROUP BY model) -
                     (SELECT AVG(rate) FROM study GROUP BY model)
                 )
-            ), 2) / (COUNT((SELECT rate FROM study GROUP BY model)) - 1)
+            ) / (COUNT((SELECT rate FROM study GROUP BY model)) - 1), 2)
         FROM
             study
         GROUP BY
