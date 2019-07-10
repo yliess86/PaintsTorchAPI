14c5553c972b713f337014dbb2cc0e7a73a553f5
Yliess <hatiyliess86@gmail.com>
2019-07-10 14:35:33 +0200

    Update models

--- api.sh
+++ api.sh
@@ -6,10 +6,8 @@ pip install -r requirements.txt
 
 python3 api.py \
   -d cuda \
-  -g '/Projects/PaintsTorchExp/trained/prs/generator.pth' \
-     '/Projects/PaintsTorchExp/trained/pss/generator.pth' \
-     '/Projects/PaintsTorchExp/trained/psd/generator.pth' \
-     '/Projects/PaintsTorchExp/trained/csd/generator.pth' \
-     '/Projects/PaintsTorchExp/custom_strokes_double/model/generator.pth' \
-  -n 'PRS' 'PSS' 'PSD' 'CSD' 'FTM' \
+  -g '/Projects/PaintsTorchExp/trained/PaperRS/generator.pth' \
+     '/Projects/PaintsTorchExp/trained/CustomSS/generator.pth' \
+     '/Projects/PaintsTorchExp/trained/CustomSD/generator.pth' \
+  -n 'PaperRS' 'CustomSS' 'CustomSD' \
   -i '/Projects/PaintsTorch/res/model/i2v.pth'
